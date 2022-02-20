ebd4ef3fbbe599a6e8e768468d738cb233b850c5
Sanjeet314 <sanjeetkumaryadav180@gmail.com>
2022-02-21 00:38:17 +0530

    backend will in memory database service

--- demo/src/main/java/com/example/demo/dao/PersonDao.java
+++ demo/src/main/java/com/example/demo/dao/PersonDao.java
@@ -3,6 +3,7 @@ package com.example.demo.dao;
 import com.example.demo.model.Person;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PersonDao {
@@ -11,8 +12,15 @@ public interface PersonDao {
 
     default int insertPerson(Person person) {
         UUID id = UUID.randomUUID();
-        return insertPerson(id, person);
+        System.out.println("first");
+        return insertPerson(id, person);//we are using method overloading concept here wisely!
     }
 
     List<Person> selectAllPeople();
+
+    Optional<Person> selectPersonById(UUID id);
+
+    int deletePersonById(UUID id);
+
+    int updatePersonById(UUID id, Person person);
 }
